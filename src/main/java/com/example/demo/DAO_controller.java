eaa45ac39e83ec85b439d3b3098ce15222334ff5
GregoryFilipson <alivenemo@gmail.com>
2021-03-28 19:32:22 +0300

    fixed code

--- src/main/java/com/example/demo/DAO_controller.java
+++ src/main/java/com/example/demo/DAO_controller.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
 
-@Controller
+@RestController
 public class DAO_controller {
     private final CustomersRepository repository;
 
@@ -16,7 +18,13 @@ public class DAO_controller {
     }
 
     @GetMapping("/products/fetch-product")
-    public List<String> getListProductName(@RequestParam("name") String name) throws IOException {
+    public List<String> getListProductName(String name) throws IOException {
         return repository.getProductName(name);
     }
+
+    @GetMapping("/test")
+    public String getListProductName() throws IOException {
+        return "Hello";
+    }
+
 }
